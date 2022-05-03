985bc588a7307248dfc615fd7652aafbaf11e0dc
priyanshsingh <priyansh@singh.com>
2022-05-03 09:57:30 +0530

    implementing Reducers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import UserResults from "../components/Users/UserResults";
+import UserSearch from "../components/Users/UserSeach";
 function Home() {
   return (
     <>
-      {/* Serach component */}
+      <UserSearch />
       <UserResults />
     </>
   );
