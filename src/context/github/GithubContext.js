0dca017fc8cf368d1dfd2fe1ba50291e92ea38ab
priyanshsingh <priyansh@singh.com>
2022-05-11 10:08:15 +0530

    Using spread operator to pass state directly

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -70,10 +70,7 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
+        ...state,
         searchUsers,
         clearUsers,
         getUser,
